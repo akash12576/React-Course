1c7ce6012d3c99b3bb9849ed2650990981a2fe32
akash12576 <ks221457@gmail.com>
2026-09-18 21:13:33 +0530

    feat: implement custom render function and add HTML structure for custom React app

--- 01basicVite/src/App.jsx
+++ 01basicVite/src/App.jsx
@@ -3,10 +3,11 @@ import Youtube from "./Youtube"
 
 function App() {
 
-
+  const username = "akash"
   return (
   <>
-   <h1>Vite React App</h1>
+   <h1>Vite React App. {2+2}</h1>
+   <h1>Vite React App. {username}</h1>
    <Youtube/>
   </>
   )
